640f1f7eb02e0fdd3aa27d1973c68fba411b529f
rlcjs131 <rlcjs131@gmail.com>
2026-09-17 07:05:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'workout-routine-pwa-';
-const CACHE_NAME = CACHE_PREFIX + 'v18-release-18';
+const CACHE_NAME = CACHE_PREFIX + 'v19-release-19';
 const BASE = '/workout-app/';
 const APP_SHELL = [BASE, BASE+'index.html', BASE+'manifest.webmanifest', BASE+'icon-192.png', BASE+'icon-512.png'];
 
